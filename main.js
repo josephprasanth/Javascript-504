3e83ae38cb2151322b07b777c467235e04933caa
josephprasanth <prasanthjoseph_77@yahoo.com>
2018-04-27 08:29:21 +0200

    first 2 questions

---
+++ main.js
@@ -0,0 +1,14 @@
+//1) Write a JavaScript program to print the contents of the current window
+var curWindow = function () {
+    window.print();
+}
+//curWindow();
+
+//2) Write a JavaScript program to get the current date. 
+//Expected Output : mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+
+var curDate = function () {
+    
+}
+//curDate();
+
